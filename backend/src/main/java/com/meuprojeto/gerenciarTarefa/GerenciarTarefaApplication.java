d6c72f8a78a708fe8a8a6101735d440a1ff7617c
Caroline Souza <loh_jg@hotmail.com>
2021-10-27 16:42:04 -0300

    criando projeto backend

---
+++ backend/src/main/java/com/meuprojeto/gerenciarTarefa/GerenciarTarefaApplication.java
@@ -0,0 +1,13 @@
+package com.meuprojeto.gerenciarTarefa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciarTarefaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciarTarefaApplication.class, args);
+	}
+
+}
