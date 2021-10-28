ce4f0963913b5fd2c9dd2ece493ffed57c07cb1e
Caroline Souza <loh_jg@hotmail.com>
2021-10-28 15:53:43 -0300

    deletando colaborador

--- backend/src/main/java/com/meuprojeto/gerenciarTarefa/services/ColaboradorService.java
+++ backend/src/main/java/com/meuprojeto/gerenciarTarefa/services/ColaboradorService.java
@@ -7,12 +7,15 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.meuprojeto.gerenciarTarefa.dto.ColaboradorDTO;
 import com.meuprojeto.gerenciarTarefa.entities.Colaborador;
 import com.meuprojeto.gerenciarTarefa.repositories.ColaboradorRepository;
+import com.meuprojeto.gerenciarTarefa.services.exceptions.DatabaseException;
 import com.meuprojeto.gerenciarTarefa.services.exceptions.ResourceNotFoundException;
 
 //registra a classe como um componente que participa da injeção de dependencia
@@ -62,6 +65,18 @@ public class ColaboradorService {
 			return new ColaboradorDTO(entity);
 		}catch(EntityNotFoundException e) {
 			throw new  ResourceNotFoundException("Id não encontrado "+id);
+		}
+		
+	}
+
+	public void delete(int id) {
+		try {
+			repository.deleteById(id);
+			
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id não encontrado "+id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Violação de integridade do banco");
 		}
 		
 	}
